076a036d3c6672e7e9994f0094961e50a8fd0f5e
Jesper Fussing Mørk <jfm@moerks.dk>
2020-10-12 13:05:42 +0200

    More testing #3

--- tests/base.py
+++ tests/base.py
@@ -4,11 +4,33 @@ from streamdeck_daemon.logging.logger import Logger
 
 class BaseTest():
 
-    def default_setup(self, mocker):
-        mocker.patch.object(Configuration, 'get_action')
+    def default_setup(self, mocker, **kwargs):
+        mocker.patch.object(Configuration, 'get_pages')
+        if 'page_config' in kwargs:
+            mocker.patch.object(Configuration, 'get_page', return_value=kwargs.get('page_config'))
+        else:
+            mocker.patch.object(Configuration, 'get_page')
+
+        if 'keys_config' in kwargs:
+            mocker.patch.object(Configuration, 'get_keys', return_value=kwargs.get('keys_config'))
+        else:
+            mocker.patch.object(Configuration, 'get_keys')
+
+        if 'key_config' in kwargs:
+            mocker.patch.object(Configuration, 'get_key', return_value=kwargs.get('key_config'))
+        else:
+            mocker.patch.object(Configuration, 'get_key')
+
+        if 'actions_config' in kwargs:
+            mocker.patch.object(Configuration, 'get_actions', return_value=kwargs.get('actions_config'))
+        else:
+            mocker.patch.object(Configuration, 'get_actions')
         mocker.patch.object(Logger, 'debug')
         self.mock_configuration = Configuration()
         self.mock_logger = Logger(self.mock_configuration)
 
     def get_action_config(self, plugin, plugin_key, plugin_value):
         return {'plugin': plugin, plugin_key: plugin_value}
+
+    def get_key_config(self, index, key_type, text, icon):
+        return {'index': index, 'type': key_type, 'text': text, 'icon': icon}
--- tests/default_actions_test.py
+++ tests/default_actions_test.py
@@ -1,8 +1,12 @@
 from streamdeck_daemon.plugins.brightness import handle_action as brightness_action
 from streamdeck_daemon.plugins.shortcut import handle_action as shortcut_action
+from streamdeck_daemon.plugins.command import handle_action as command_action
+from streamdeck_daemon.plugins.launch import handle_action as launch_action
+from streamdeck_daemon.plugins.page import handle_action as page_action
 from streamdeck_daemon.handlers.view_handler import ViewHandler
 from tests.base import BaseTest
 import pyautogui
+import os
 
 
 class TestDefaultActions(BaseTest):
@@ -29,3 +33,37 @@ class TestDefaultActions(BaseTest):
         shortcut_action(self.mock_logger, None, action_config)
 
         assert pyautogui.hotkey.called_once_with('shift+t')
+
+    def test_command_action(self, mocker):
+        self.default_setup(mocker)
+
+        mocker.patch('os.system')
+
+        action_config = self.get_action_config('command', 'command', 'ls')
+
+        command_action(self.mock_logger, None, action_config)
+
+        assert os.system.called_once_with('ls')
+
+    def test_launch_action(self, mocker):
+        self.default_setup(mocker)
+
+        mocker.patch('os.system')
+
+        action_config = self.get_action_config('launch', 'application', 'twitter')
+
+        launch_action(self.mock_logger, None, action_config)
+
+        assert os.system.called_once_with('gtk-launch twitter')
+
+    def test_page_action(self, mocker):
+        self.default_setup(mocker)
+
+        mocker.patch.object(ViewHandler, 'render_page')
+        mock_view_handler = ViewHandler(self.mock_configuration, self.mock_logger, None, None)
+
+        action_config = self.get_action_config('page', 'page', 'testpage')
+
+        page_action(self.mock_logger, mock_view_handler, action_config)
+
+        mock_view_handler.render_page.assert_called_once_with('testpage')
