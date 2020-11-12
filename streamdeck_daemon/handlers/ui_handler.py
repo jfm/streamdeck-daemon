d08b46a23697e7ca6f58a38ebe47c289019ed5fa
Jesper Fussing Mørk <jfm@moerks.dk>
2020-11-12 07:15:45 +0100

    Various small fixes

--- streamdeck_daemon/handlers/ui_handler.py
+++ streamdeck_daemon/handlers/ui_handler.py
@@ -7,6 +7,7 @@ class UIHandler(object):
         self.config = config
         self.logger = logger
         self.toggle_map = {}
+        self.font = ImageFont.truetype('/usr/share/fonts/TTF/DejaVuSansMono.ttf', 14)
 
     def render_page(self, streamdeck, page_name):
         pages_config = self.config.get_pages()
@@ -18,15 +19,12 @@ class UIHandler(object):
             self.render_key_image(streamdeck, page_name, index, icon, text)
 
     def render_key_image(self, streamdeck, page_name, key_number, icon_path, text):
-        pages_config = self.config.get_pages()
-        page_config = self.config.get_page(pages_config, page_name)
         icon = Image.open(icon_path)
         image = PILHelper.create_scaled_image(streamdeck, icon, margins=[0, 0, 20, 0])
         draw = ImageDraw.Draw(image)
-        font = ImageFont.truetype(page_config['font'], page_config['font-size'])
-        label_w, label_h = draw.textsize(text, font=font)
+        label_w, label_h = draw.textsize(text, font=self.font)
         label_pos = ((image.width - label_w) // 2, image.height - 20)
-        draw.text(label_pos, text=text, font=font, fill="white")
+        draw.text(label_pos, text=text, font=self.font, fill="white")
         rendered_image = PILHelper.to_native_format(streamdeck, image)
         with streamdeck:
             streamdeck.set_key_image(key_number, rendered_image)
--- streamdeck_daemon/plugins/obs.py
+++ streamdeck_daemon/plugins/obs.py
@@ -5,7 +5,9 @@ import simpleobsws
 loop = asyncio.new_event_loop()
 obs = simpleobsws.obsws(loop=loop)
 request_map = {
-        'change_scene': 'SetCurrentScene'
+        'change_scene': 'SetCurrentScene',
+        'toggle_recording': 'StartStopRecording',
+        'toggle_streaming': 'StartStopStreaming'
 }
 
 
@@ -16,14 +18,15 @@ def handle_action(logger, view_handler, action_config):
 def get_request_dictionary(action_config):
     if action_config['request'] == 'change_scene':
         return {'scene-name': action_config['scene']}
+    else:
+        return {}
 
 
 async def handle_action_async(logger, view_handler, action_config):
     await obs.connect()
     request = request_map.get(action_config['request'])
     request_data = get_request_dictionary(action_config)
-    result = await obs.call(request, data=request_data)
-    print(result)
+    await obs.call(request, data=request_data)
     await obs.disconnect()
     return 'success'
 
